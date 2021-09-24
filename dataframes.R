899196e640a07dc91039bf8b5258c9e35c8ffc6e
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-09-24 13:52:10 +0600

    conditional sub-setting codes added in dataframes.R

--- dataframes.R
+++ dataframes.R
@@ -14,3 +14,20 @@ employees_data[1,"startdate"]
 employees_data$startdate
 employees_data[1,]
 
+my_dataset <- data.frame(site   = c("A", "B", "A", "A", "B"),
+                         seasons= c("Winter", "Summer", "Summer", "Spring", "Fall"),
+                         pH     = c(7.4, 6.3, 8.6, 7.2, 8.9),
+                         stringsAsFactors = FALSE)
+my_dataset[3, 2]
+which(colnames(my_dataset) != "site")
+
+# conditional subsetting
+# always takes 2 parameters X & Y such as [X, Y]
+my_dataset$pH > 7                               # creates a vector with pH > 7
+my_dataset[my_dataset$pH > 7, ]                 #
+my_dataset[c(TRUE, FALSE, TRUE, TRUE, TRUE), ]  # does the same thing as following
+my_dataset[my_dataset$pH > 7, "site"]           #
+
+my_dataset[my_dataset$seasons == "Summer", ]
+my_dataset[my_dataset$seasons == "Summer", c("site", "pH")]
+my_dataset[my_dataset$seasons == "Summer", which(colnames(my_dataset) != "seasons")]
